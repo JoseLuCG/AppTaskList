af08d5a07295fc8ec891c74715d16f3a4dda1d17
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-08 11:11:31 +0100

    Añade Documentacion a domainObjects

--- src/js/models/domainObjects.mjs
+++ src/js/models/domainObjects.mjs
@@ -3,19 +3,32 @@ import { updateTasksHTML } from "../controllers/tasks.mjs";
 
 export const tasksStorageKey = "tasks"
 
+/**
+ * Toma el valor guardado en la StorageKey(en este caso "tasks") y los transforma en datos.
+ * @returns {array} - Array con los objetos de las tareas.
+ */
 export function getTasks () {
     const stringData = localStorage.getItem(tasksStorageKey) || "[]";
     return JSON.parse(stringData);
 }
 
+/**
+ * Mete una tarea dentro del array.
+ * @param {object} taskObject - Objeto a añadir a tarea.
+ */
 export function addTask(taskObject) {
     const tasks = getTasks();
     tasks.push(taskObject);
     saveTasks(tasks);
 }
 
+/**
+ * Cambia los datos a tipo "string", para poder guardarlos
+ * en LocalStorage.
+ * @param {Array} newTasksArray Array a transformar en string para guardar en LocalStorage.
+ */
 export function saveTasks(newTasksArray) {
     const stringData = JSON.stringify(newTasksArray)
     localStorage.setItem(tasksStorageKey, stringData);
-    updateTasksHTML(taskListHTMLSelector, newTasksArray)
+    updateTasksHTML(taskListHTMLSelector, newTasksArray);
 }
