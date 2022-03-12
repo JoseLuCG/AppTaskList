f222f0c511987c5d13ebe6ac37cf63d92b7af98c
JoseLuis <joseluisconde@pretonmail.com>
2022-03-12 18:02:40 +0100

    Colocacion del codigo

--- src/js/controllers/tasks.mjs
+++ src/js/controllers/tasks.mjs
@@ -1,6 +1,5 @@
 import { addTask, saveTasks, getTasks, deleteAllCompletedTasksHandler } from "../models/domainObjects.mjs";
 import { disappear, SearchTaskTextSelector, taskListHTMLSelector, addTaskInputSelector, completedCSSClass, buttonDeleteAllCompletedTasks, SearchTaskButtonSelector } from "../models/defines.mjs"
-
 /**
  * Transforma los datos a HTML
  * @param {*} taskIndex 
@@ -15,8 +14,6 @@ export function task2HTMLElement (taskIndex, taskObject) {
     const inputCheckboxDeleteHTMLItem = document.createElement("input");
     const buttonUno=document.querySelector("#buttonUno");
     buttonUno.addEventListener("click",hideHandler);
-    
-
     const buttonEditHTMLItem = document.createElement("button");
     // Les proporciono valores 
     inputCheckboxHTMLItem.type = "checkbox";
@@ -36,9 +33,6 @@ export function task2HTMLElement (taskIndex, taskObject) {
         listHTMLItem.classList.remove(completedCSSClass);
         listHTMLItem.append(buttonEditHTMLItem);
     }
-
-    
-
     // Añado el manejador de eventos para el checkbox que selecciona(el primero)
     inputCheckboxHTMLItem.addEventListener(
         "click",
@@ -48,29 +42,22 @@ export function task2HTMLElement (taskIndex, taskObject) {
             saveTasks(tasks);
         }
     );
-
 //*TODO Modificar el boton para saber cual es el que borra y el de tarea completada*/
 
     // Añado el manejador de eventos para el checkbox que borra la tarea(el segundo)
     // Este checkbox borra el elemento del array de tareas y del HTML
-    
       inputCheckboxDeleteHTMLItem.addEventListener(
         "click",
         (event) => {
             //obtengo el array de tareas del LocalStorage
             const tasks = getTasks();
-            //si la posición (index) está en el rango array, borra el objeto del 
-            //array en esa posición
-            if ( taskIndex >-1 && taskIndex<=tasks.length) 
-                tasks.splice( taskIndex, 1 );
-            //actualiza el HTML y el localStorage con el array de tareas ya modificado
+            //si la posición (index) está en el rango array, borra el objeto del array en esa posición. 
+            tasks.splice( taskIndex, 1 );
             saveTasks(tasks);
         }
     );
-
     return listHTMLItem
 }
-
 /**
  * Manejador del evento del click del boton ver/ocultar 
  * tareas completadas 
@@ -83,11 +70,6 @@ function hideHandler(event){
         item.classList.toggle("visibilidad");
 
 }
-
-
-//Añade el manejador de eventos para el checkbox que borra todas las tareas completadas.
-document.querySelector(buttonDeleteAllCompletedTasks).addEventListener("click", deleteAllCompletedTasksHandler);
-
 /**
  * Comprueba los elementos del array y los muestra en pantalla. 
  * @param {*} CSSselector - Seria un ul
@@ -105,7 +87,6 @@ export function updateTasksHTML (CSSselector, tasksArray) {
     }
     orderCompletedTask (listHTMLElement)
 }
-
 /**
  * Añade un nuevo objeto al array de tareas.
  * @param {*} event 
@@ -125,7 +106,6 @@ export function taskAddButtonClickHandler (event) {
         window.alert("Añade una descripción a la tarea antes de continuar");
     }
 }
-
 /**
  * Agrupa las tareas completadas a final de la lista.
  *  @param {object} ul Es el ul de HTML.
@@ -135,15 +115,11 @@ export function taskAddButtonClickHandler (event) {
 
     const ul = document.querySelector("ul");
     const li = document.querySelectorAll(".completed");
-    //console.log(ul, li);
-
     for (let idx = 0; idx < li.length; idx++) {
         const checkbox = li[idx];
-        //console.log(checkbox);
         ul.appendChild(checkbox);
     }
 };
-
 /**
  * Manejador del evento del click del boton de buscar tareas
  * @param {undefined} event 
@@ -153,13 +129,11 @@ export function SearchTaskButtonClickHandler(event){
     let expresionABuscar = document.querySelector(SearchTaskTextSelector).value
     console.log("la expresion a buscar es: " + expresionABuscar)
     const resultado=tasks.forEach(element => {
-    element.taskName.match(expresionABuscar);
-    
+    element.taskName.match(expresionABuscar);    
     });
     console.log(resultado);
     
 }
-
 /**
  * Modifica el texto de la tarea.
  * @param {*} params 
@@ -177,3 +151,6 @@ function editTasks (event) {
 
 }
 */
+
+//Añade el manejador de eventos para el checkbox que borra todas las tareas completadas.
+document.querySelector(buttonDeleteAllCompletedTasks).addEventListener("click", deleteAllCompletedTasksHandler);
--- src/js/models/domainObjects.mjs
+++ src/js/models/domainObjects.mjs
@@ -2,7 +2,6 @@ import { taskListHTMLSelector } from "./defines.mjs";
 import { updateTasksHTML } from "../controllers/tasks.mjs";
 
 export const tasksStorageKey = "tasks"
-
 /**
  * Toma el valor guardado en la StorageKey(en este caso "tasks") y los transforma en datos.
  * @returns {array} - Array con los objetos de las tareas.
@@ -11,7 +10,6 @@ export function getTasks () {
     const stringData = localStorage.getItem(tasksStorageKey) || "[]";
     return JSON.parse(stringData);
 }
-
 /**
  * Mete una tarea dentro del array.
  * @param {object} taskObject - Objeto a añadir a tarea.
@@ -21,7 +19,6 @@ export function addTask(taskObject) {
     tasks.push(taskObject);
     saveTasks(tasks);
 }
-
 /**
  * Cambia los datos a tipo "string", para poder guardarlos
  * en LocalStorage.
@@ -32,7 +29,6 @@ export function saveTasks(newTasksArray) {
     localStorage.setItem(tasksStorageKey, stringData);
     updateTasksHTML(taskListHTMLSelector, newTasksArray)
 }
-
 /**
  * Elimina todas las tareas completadas.
  */
